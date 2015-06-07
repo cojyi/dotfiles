34bde657f51dff6dd9c4c283ea5a808e53f5aa61
cojyi <coj4yi@gmail.com>
2015-06-06 22:40:51 -0400

    Another commit

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_profile functions minttyrc vimrc README.md"    # list of files/folders to symlink in homedir
+files="bash_profile functions minttyrc vimrc vim README.md"    # list of files/folders to symlink in homedir
 
 ##########
 
